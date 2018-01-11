48a9ef47ba7208965c30d3369daeee1d03f77eb3
Richard Feldman <richard.t.feldman@gmail.com>
2018-01-11 12:59:04 -0500

    Add Environment, fix POST format

--- src/Rollbar.elm
+++ src/Rollbar.elm
@@ -1,16 +1,21 @@
-module Rollbar exposing (Level(..), Rollbar, scoped, send)
+module Rollbar exposing (Environment, Level(..), Rollbar, Scope, Token, environment, scope, scoped, send, token)
 
 import Http
 import Json.Encode exposing (Value)
 import Task exposing (Task)
 
 
+version : String
+version =
+    "1.0.0"
+
+
 type alias Rollbar =
-    { critical : String -> Value -> Task Http.Error ()
-    , error : String -> Value -> Task Http.Error ()
-    , warning : String -> Value -> Task Http.Error ()
-    , info : String -> Value -> Task Http.Error ()
-    , debug : String -> Value -> Task Http.Error ()
+    { critical : String -> List ( String, Value ) -> Task Http.Error ()
+    , error : String -> List ( String, Value ) -> Task Http.Error ()
+    , warning : String -> List ( String, Value ) -> Task Http.Error ()
+    , info : String -> List ( String, Value ) -> Task Http.Error ()
+    , debug : String -> List ( String, Value ) -> Task Http.Error ()
     }
 
 
@@ -26,12 +31,31 @@ type Token
     = Token String
 
 
+token : String -> Token
+token =
+    Token
+
+
 type Scope
     = Scope String
 
 
-toStringReportType : Level -> String
-toStringReportType report =
+scope : String -> Scope
+scope =
+    Scope
+
+
+type Environment
+    = Environment String
+
+
+environment : String -> Environment
+environment =
+    Environment
+
+
+levelToString : Level -> String
+levelToString report =
     case report of
         Critical ->
             "critical"
@@ -49,12 +73,17 @@ toStringReportType report =
             "warning"
 
 
-send : Token -> Scope -> Level -> String -> Value -> Task Http.Error ()
-send token scope level message payload =
+endpointUrl : String
+endpointUrl =
+    "https://api.rollbar.com/api/1/item/"
+
+
+send : Token -> Scope -> Environment -> Level -> String -> List ( String, Value ) -> Task Http.Error ()
+send token scope environment level message metadata =
     { method = "POST"
     , headers = [ tokenHeader token ]
-    , url = "https://api.rollbar.com/api/1/item"
-    , body = Http.jsonBody payload
+    , url = endpointUrl
+    , body = toJsonBody token environment level message metadata
     , expect = Http.expectStringResponse (\_ -> Ok ()) -- TODO
     , timeout = Nothing
     , withCredentials = False
@@ -62,6 +91,38 @@ send token scope level message payload =
         |> Http.request
         -- TODO retry if rate limited
         |> Http.toTask
+
+
+toJsonBody : Token -> Environment -> Level -> String -> List ( String, Value ) -> Http.Body
+toJsonBody (Token token) (Environment environment) level message metadata =
+    -- See https://rollbar.com/docs/api/items_post/ for schema
+    [ ( "access_token", Json.Encode.string token )
+    , ( "data"
+      , Json.Encode.object
+            [ ( "environment", Json.Encode.string environment )
+            , ( "notifier"
+              , Json.Encode.object
+                    [ ( "name", Json.Encode.string "elm-rollbar" )
+                    , ( "version", Json.Encode.string version )
+                    ]
+              )
+            , ( "level", Json.Encode.string (levelToString level) )
+            , ( "endpoint", Json.Encode.string endpointUrl )
+            , ( "platform", Json.Encode.string "browser" )
+            , ( "language", Json.Encode.string "Elm" )
+            , ( "body"
+              , Json.Encode.object
+                    [ ( "message"
+                      , Json.Encode.object
+                            (( "body", Json.Encode.string message ) :: metadata)
+                      )
+                    ]
+              )
+            ]
+      )
+    ]
+        |> Json.Encode.object
+        |> Http.jsonBody
 
 
 tokenHeader : Token -> Http.Header
@@ -79,15 +140,15 @@ to take ExtraInfo for both items above and below the error level.
     rollbar.error "Unexpected payload from the hats API." [ ("Payload", toString payload) ]
 
 -}
-scoped : Token -> String -> Rollbar
-scoped token scopeStr =
+scoped : Token -> Environment -> String -> Rollbar
+scoped token environment scopeStr =
     let
         scope =
             Scope scopeStr
     in
-    { critical = send token scope Critical
-    , error = send token scope Error
-    , warning = send token scope Warning
-    , info = send token scope Info
-    , debug = send token scope Debug
+    { critical = send token scope environment Critical
+    , error = send token scope environment Error
+    , warning = send token scope environment Warning
+    , info = send token scope environment Info
+    , debug = send token scope environment Debug
     }
